c4774c876b3d8212589cf7d5b5131b407ebb80ed
Zarek <simen1127@gmail.com>
2018-01-04 21:30:49 +0800

    user show page,update the profile detailes, upload avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_restaurant, only: [:edit, :update]
+  
+  def show
+    @users = User.all 
+  end
+ 
+
+  def update
+   if @users.update_attributes(users_params)    
+      flash[:notice] = "users was scuccessfully updated"
+      redirect_to user_path(@users)  
+    else
+      flash.now[:alert] ="users was failed to update"
+      render :edit
+    end  
+  end
+
+  private
+
+  def set_restaurant
+    @users = User.find(params[:id])    
+  end
+
+  def users_params
+    params.require(:user).permit(:name, :intro, :avatar)  
+  end
+
 end
---
+++ db/migrate/20171227110727_create_drop_tables.rb
@@ -0,0 +1,5 @@
+class CreateDropTables < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :comments
+  end
+end
