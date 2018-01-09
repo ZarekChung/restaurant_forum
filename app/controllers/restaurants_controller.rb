3126958c0893a6211d2c686f262e12a53ff97963
Zarek <simen1127@gmail.com>
2018-01-09 21:31:48 +0800

    -user can like to dashboard from restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,7 +13,10 @@ class RestaurantsController < ApplicationController
   def feeds
     @recent_restaurants = Restaurant.order(created_at: :desc).limit(10)
     @recent_comments = Comment.order(created_at: :desc).limit(10)
-    
+  end
+
+  def dashboard
+    @restaurant = Restaurant.find(params[:id])
   end
   
 end
